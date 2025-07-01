dab5680182ca01daaaf3a0569cece0a2f5621a87
princ <113239025+VGITA777@users.noreply.github.com>
2025-07-01 18:13:13 +0800

    Add TV series fields to SearchMultiDataModel for multi-category search

--- src/main/java/com/prince/movieezapi/api/models/search/SearchMultiDataModel.java
+++ src/main/java/com/prince/movieezapi/api/models/search/SearchMultiDataModel.java
@@ -7,7 +7,13 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class SearchMultiDataModel extends MediaShortDetailsWithMediaTypeModel {
+    // For movies
     private String title;
     private String original_title;
     private String release_date;
+
+    // For TV series
+    private String name;
+    private String original_name;
+    private String first_air_date;
 }
