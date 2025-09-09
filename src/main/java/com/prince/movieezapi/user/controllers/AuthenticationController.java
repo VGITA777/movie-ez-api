30bfd3ed2016691824becda476c7976d5251dce8
VGITA777 <113239025+VGITA777@users.noreply.github.com>
2025-09-09 14:45:42 +0800

    Add input validation for email and password in login endpoint

--- src/main/java/com/prince/movieezapi/user/controllers/AuthenticationController.java
+++ src/main/java/com/prince/movieezapi/user/controllers/AuthenticationController.java
@@ -1,7 +1,11 @@
 package com.prince.movieezapi.user.controllers;
 
+import com.prince.movieezapi.shared.UserSecurityUtils;
+import com.prince.movieezapi.user.exceptions.MalformedEmailException;
+import com.prince.movieezapi.user.exceptions.MalformedPasswordException;
 import com.prince.movieezapi.user.inputs.EmailPasswordInput;
 import com.prince.movieezapi.user.services.UserAuthenticationService;
+import lombok.SneakyThrows;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,8 +22,19 @@ public class AuthenticationController {
         this.userAuthenticationService = userAuthenticationService;
     }
 
+    @SneakyThrows
+    public static void validateEmailPasswordInput(EmailPasswordInput input) {
+        if (!UserSecurityUtils.isEmailValid(input.email())) {
+            throw new MalformedEmailException("Malformed email");
+        }
+        if (!UserSecurityUtils.isPasswordValid(input.password())) {
+            throw new MalformedPasswordException("Malformed password");
+        }
+    }
+
     @PostMapping("/login")
     public ResponseEntity<?> emailPasswordLogin(@RequestBody EmailPasswordInput input) {
+        validateEmailPasswordInput(input);
         return userAuthenticationService.authenticateUserWithEmail(input.email(), input.password());
     }
 }
