2d1c62f98dab7d8beef3968a97c26dddc5018925
VGITA777 <113239025+VGITA777@users.noreply.github.com>
2025-09-09 14:30:02 +0800

    Add custom exceptions for malformed email and password

---
+++ src/main/java/com/prince/movieezapi/user/exceptions/MalformedEmailException.java
@@ -0,0 +1,7 @@
+package com.prince.movieezapi.user.exceptions;
+
+public class MalformedEmailException extends Throwable {
+    public MalformedEmailException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/prince/movieezapi/user/exceptions/MalformedPasswordException.java
@@ -0,0 +1,7 @@
+package com.prince.movieezapi.user.exceptions;
+
+public class MalformedPasswordException extends Throwable {
+    public MalformedPasswordException(String message) {
+        super(message);
+    }
+}
