3931118943112fe35b6b834bc76bd178a8500236
horizon <113239025+VGITA777@users.noreply.github.com>
2025-11-03 23:14:26 +0800

    Update AlphanumericValidator to support Unicode characters and single internal spaces

--- src/main/java/com/prince/movieezapi/user/validators/AlphanumericValidator.java
+++ src/main/java/com/prince/movieezapi/user/validators/AlphanumericValidator.java
@@ -6,10 +6,10 @@ import jakarta.validation.ConstraintValidatorContext;
 
 public class AlphanumericValidator implements ConstraintValidator<Alphanumeric, String> {
 
-    private static final String ALPHANUMERIC_REGEX = "^[a-zA-Z0-9]+$";
+    private static final String UNICODE_ALNUM_SINGLE_INTERNAL_SPACES = "^[\\p{L}\\p{N}]+(?: [\\p{L}\\p{N}]+)*$";
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        return value != null && value.matches(ALPHANUMERIC_REGEX);
+        return value != null && value.matches(UNICODE_ALNUM_SINGLE_INTERNAL_SPACES);
     }
 }
