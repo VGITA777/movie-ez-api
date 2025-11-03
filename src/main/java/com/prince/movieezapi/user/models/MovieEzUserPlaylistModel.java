a42446cc5a189ce43db0539987de3b94040222d7
horizon <113239025+VGITA777@users.noreply.github.com>
2025-11-03 17:51:34 +0800

    Enhance MovieEzUserPlaylistModel with content management methods and uniqueness constraint for playlist names

--- src/main/java/com/prince/movieezapi/user/models/MovieEzUserPlaylistModel.java
+++ src/main/java/com/prince/movieezapi/user/models/MovieEzUserPlaylistModel.java
@@ -3,16 +3,14 @@ package com.prince.movieezapi.user.models;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@EqualsAndHashCode
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -25,13 +23,16 @@ public class MovieEzUserPlaylistModel {
     @UuidGenerator
     private UUID id;
 
+    // MAKE SURE THAT EACH USER ONLY HAS ONE PLAYLIST WITH THE SAME NAME
     @Column(nullable = false)
     private String name;
 
+    @EqualsAndHashCode.Exclude
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private MovieEzUserModel user;
 
+    @EqualsAndHashCode.Exclude
     @OneToMany(mappedBy = "playlist", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
     @Builder.Default
     private List<MovieEzPlaylistContentsModel> contents = new ArrayList<>();
@@ -41,8 +42,26 @@ public class MovieEzUserPlaylistModel {
         this.contents.add(content);
     }
 
+    public void addContent(List<MovieEzPlaylistContentsModel> contentList) {
+        List<MovieEzPlaylistContentsModel> playlistToBeInserted = contentList.stream()
+                .filter(e -> {
+                    if (e == null) {
+                        return false;
+                    }
+                    String trackId = e.getMediaId();
+                    boolean isTrackIdInContents = contents.stream().anyMatch(c -> c.getMediaId().equals(trackId));
+                    return !isTrackIdInContents;
+                })
+                .peek(e -> e.setPlaylist(this))
+                .toList();
+        this.contents.addAll(playlistToBeInserted);
+    }
+
     public void removeContent(MovieEzPlaylistContentsModel content) {
-        content.setPlaylist(null);
         this.contents.remove(content);
+    }
+
+    public void removeContent(List<String> contentList) {
+        this.contents.removeIf(e -> contentList.contains(e.getMediaId()));
     }
 }
