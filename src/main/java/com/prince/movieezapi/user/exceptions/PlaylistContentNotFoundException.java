c169337285cdd77b873fb08dbd3d3312440c7fc8
horizon <113239025+VGITA777@users.noreply.github.com>
2025-11-09 02:44:13 +0800

    refactor: change PlaylistContentNotFoundException to extend NotFoundException

--- src/main/java/com/prince/movieezapi/user/exceptions/PlaylistContentNotFoundException.java
+++ src/main/java/com/prince/movieezapi/user/exceptions/PlaylistContentNotFoundException.java
@@ -1,6 +1,6 @@
 package com.prince.movieezapi.user.exceptions;
 
-public class PlaylistContentNotFoundException extends RuntimeException {
+public class PlaylistContentNotFoundException extends NotFoundException {
     public PlaylistContentNotFoundException(String message) {
         super(message);
     }
