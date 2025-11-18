5f8f47b1e38e606f76336fff09e66f670384352b
horizon <113239025+VGITA777@users.noreply.github.com>
2025-11-19 01:04:01 +0800

    Add JSON response for successful email sending in OttSendMailGenerationSuccessHandlerService

--- src/main/java/com/prince/movieezapi/security/ott/OttSendMailGenerationSuccessHandlerService.java
+++ src/main/java/com/prince/movieezapi/security/ott/OttSendMailGenerationSuccessHandlerService.java
@@ -1,6 +1,7 @@
 package com.prince.movieezapi.security.ott;
 
 import com.prince.movieezapi.security.models.OttMailModel;
+import com.prince.movieezapi.shared.models.responses.ServerAuthenticationResponse;
 import com.prince.movieezapi.shared.utilities.BasicUtils;
 import com.prince.movieezapi.user.services.MovieEzUserService;
 import jakarta.servlet.ServletException;
@@ -8,6 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.ott.OneTimeToken;
 import org.springframework.security.web.authentication.ott.OneTimeTokenGenerationSuccessHandler;
 import org.springframework.stereotype.Service;
@@ -50,6 +52,7 @@ public class OttSendMailGenerationSuccessHandlerService implements OneTimeTokenG
 
         try {
             ottMailSenderService.sendMail(mailMessage);
+            basicUtils.sendJson(HttpStatus.OK, ServerAuthenticationResponse.success("Email sent successfully", null), response);
             log.info("Email sent successfully to user: '{}'", userIdentifier);
         } catch (Exception _) {
             log.info("Failed to send email to user: '{}'", userIdentifier);
