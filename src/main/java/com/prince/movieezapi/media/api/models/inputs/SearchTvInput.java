6cfc0d50f6385030378668f4b0c6143bc1663426
horizon <113239025+VGITA777@users.noreply.github.com>
2025-11-22 03:29:02 +0800

    import optimizations

--- src/main/java/com/prince/movieezapi/media/api/models/inputs/SearchTvInput.java
+++ src/main/java/com/prince/movieezapi/media/api/models/inputs/SearchTvInput.java
@@ -1,6 +1,9 @@
 package com.prince.movieezapi.media.api.models.inputs;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
