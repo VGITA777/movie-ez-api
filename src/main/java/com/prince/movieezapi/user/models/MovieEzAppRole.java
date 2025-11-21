ff59c733fa2ce416883cc239f252c0d5939d4834
horizon <113239025+VGITA777@users.noreply.github.com>
2025-11-22 01:17:25 +0800

    Enhance MovieEzAppRole enum with priority values and getter method

--- src/main/java/com/prince/movieezapi/user/models/MovieEzAppRole.java
+++ src/main/java/com/prince/movieezapi/user/models/MovieEzAppRole.java
@@ -1,10 +1,19 @@
 package com.prince.movieezapi.user.models;
 
+import lombok.Getter;
+
 /**
  * An enum of available roles.
  */
+@Getter
 public enum MovieEzAppRole {
-    GUEST,
-    USER,
-    ADMIN
+    GUEST(0),
+    USER(1),
+    ADMIN(Integer.MAX_VALUE);
+
+    private final int priority;
+
+    MovieEzAppRole(int priority) {
+        this.priority = priority;
+    }
 }
