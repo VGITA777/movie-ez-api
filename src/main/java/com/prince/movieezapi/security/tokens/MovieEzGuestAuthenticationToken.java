d02f47d43b02fd941c0c195bd3fca5fdc2b9d032
horizon <113239025+VGITA777@users.noreply.github.com>
2026-02-08 00:23:11 +0800

    Created Guest token for /media/** endpoint

---
+++ src/main/java/com/prince/movieezapi/security/tokens/MovieEzGuestAuthenticationToken.java
@@ -0,0 +1,29 @@
+package com.prince.movieezapi.security.tokens;
+
+import com.prince.movieezapi.security.ratelimit.RateLimiterUserRoles;
+import java.util.List;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+public class MovieEzGuestAuthenticationToken extends AbstractAuthenticationToken {
+
+  public MovieEzGuestAuthenticationToken() {
+    super(List.of(new SimpleGrantedAuthority("ROLE_" + RateLimiterUserRoles.GUEST.name())));
+  }
+
+  @Override
+  public @Nullable Object getCredentials() {
+    return null;
+  }
+
+  @Override
+  public @Nullable Object getPrincipal() {
+    return null;
+  }
+
+  @Override
+  public boolean isAuthenticated() {
+    return true;
+  }
+}
