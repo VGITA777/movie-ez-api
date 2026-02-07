da85400b8f022347eb08a36574f96b9312a0bbcf
horizon <113239025+VGITA777@users.noreply.github.com>
2026-02-08 00:34:12 +0800

    updated security utils

--- src/main/java/com/prince/movieezapi/shared/utilities/SecurityUtils.java
+++ src/main/java/com/prince/movieezapi/shared/utilities/SecurityUtils.java
@@ -1,12 +1,12 @@
 package com.prince.movieezapi.shared.utilities;
 
-import com.prince.movieezapi.security.authenticationtokens.MovieEzFullyAuthenticatedUser;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextHolderStrategy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 
 public class SecurityUtils {
@@ -19,7 +19,7 @@ public class SecurityUtils {
 
   public static boolean isAuthenticated() {
     Authentication authentication = CONTEXT_HOLDER_STRATEGY.getContext().getAuthentication();
-    return authentication instanceof MovieEzFullyAuthenticatedUser;
+    return authentication instanceof JwtAuthenticationToken;
   }
 
   public static void setCurrentAuthentication(
