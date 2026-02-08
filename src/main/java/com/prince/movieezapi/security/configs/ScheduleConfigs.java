7f9cc409aaf3d60cc3d5bbd4fa93a9a1ef96a601
horizon <113239025+VGITA777@users.noreply.github.com>
2026-02-08 18:36:38 +0800

    enabled scheduling

---
+++ src/main/java/com/prince/movieezapi/security/configs/ScheduleConfigs.java
@@ -0,0 +1,10 @@
+package com.prince.movieezapi.security.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class ScheduleConfigs {
+
+}
