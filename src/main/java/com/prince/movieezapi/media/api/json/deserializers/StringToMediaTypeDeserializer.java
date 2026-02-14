91194254caafa2c675e15a8e2054c0b616cb4738
horizon <113239025+VGITA777@users.noreply.github.com>
2026-02-14 20:48:17 +0800

    Updated serializers and deserializers

--- src/main/java/com/prince/movieezapi/media/api/json/deserializers/StringToMediaTypeDeserializer.java
+++ src/main/java/com/prince/movieezapi/media/api/json/deserializers/StringToMediaTypeDeserializer.java
@@ -2,21 +2,18 @@ package com.prince.movieezapi.media.api.json.deserializers;
 
 import com.prince.movieezapi.media.api.models.enums.MediaType;
 import lombok.NonNull;
-import org.springframework.boot.jackson.ObjectValueDeserializer;
+import tools.jackson.core.JacksonException;
 import tools.jackson.core.JsonParser;
 import tools.jackson.databind.DeserializationContext;
-import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ValueDeserializer;
 
-public class StringToMediaTypeDeserializer extends ObjectValueDeserializer<@NonNull MediaType> {
+public class StringToMediaTypeDeserializer extends ValueDeserializer<@NonNull MediaType> {
 
   @Override
-  protected MediaType deserializeObject(
-      JsonParser jsonParser,
-      @NonNull
-      DeserializationContext context,
-      @NonNull
-      JsonNode tree
-  ) {
-    return MediaType.fromValue(jsonParser.getString().trim().toLowerCase());
+  public @NonNull MediaType deserialize(JsonParser p, DeserializationContext ctxt) throws JacksonException {
+    return MediaType.fromValue(p
+                                   .getString()
+                                   .trim()
+                                   .toLowerCase());
   }
 }
--- src/main/java/com/prince/movieezapi/media/api/json/serailizers/MediaTypeToStringSerializer.java
+++ src/main/java/com/prince/movieezapi/media/api/json/serailizers/MediaTypeToStringSerializer.java
@@ -2,15 +2,18 @@ package com.prince.movieezapi.media.api.json.serailizers;
 
 import com.prince.movieezapi.media.api.models.enums.MediaType;
 import lombok.NonNull;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.boot.jackson.ObjectValueSerializer;
+import tools.jackson.core.JacksonException;
 import tools.jackson.core.JsonGenerator;
 import tools.jackson.databind.SerializationContext;
+import tools.jackson.databind.ValueSerializer;
 
-public class MediaTypeToStringSerializer extends ObjectValueSerializer<@NonNull MediaType> {
+public class MediaTypeToStringSerializer extends ValueSerializer<MediaType> {
 
   @Override
-  protected void serializeObject(MediaType value, JsonGenerator jgen, @NotNull SerializationContext context) {
-    jgen.writeString(value.toString().toLowerCase());
+  public void serialize(
+      @NonNull
+      MediaType value, JsonGenerator gen, SerializationContext ctxt
+  ) throws JacksonException {
+    gen.writeString(value.toString());
   }
 }
