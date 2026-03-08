d70fb232175efea7274ef26b02258e9286d447b7
horizon <113239025+VGITA777@users.noreply.github.com>
2026-03-08 16:22:16 +0800

    feat: add client-id property to Keycloak configuration and added variable for controlling profiles

--- src/main/java/com/prince/movieezapi/security/keycloak/KeycloakAdminConfigurationProperties.java
+++ src/main/java/com/prince/movieezapi/security/keycloak/KeycloakAdminConfigurationProperties.java
@@ -10,8 +10,9 @@ public class KeycloakAdminConfigurationProperties {
   private final String secret;
   private final String realm;
   private final String serverUrl;
+  private final String clientId;
 
-  public KeycloakAdminConfigurationProperties(String serverUrl, String realm, String secret) {
+  public KeycloakAdminConfigurationProperties(String serverUrl, String realm, String secret, String clientId) {
     if (serverUrl == null || serverUrl.isBlank()) {
       throw new IllegalArgumentException("Property 'serverUrl' cannot be null or empty");
     }
@@ -21,9 +22,13 @@ public class KeycloakAdminConfigurationProperties {
     if (secret == null || secret.isBlank()) {
       throw new IllegalArgumentException("Property 'secret' cannot be null or empty");
     }
+    if (clientId == null || clientId.isBlank()) {
+      throw new IllegalArgumentException("Property 'clientId' cannot be null or empty");
+    }
 
     this.serverUrl = serverUrl;
     this.realm = realm;
     this.secret = secret;
+    this.clientId = clientId;
   }
 }
